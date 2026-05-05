3111c5379bb7450164c82ff32c47bc6ee8f01eb1
quique <enriquesalceda@gmail.com>
2026-05-05 23:49:08 +1000

    Revert "add About scene + lift makeButton into shared module"
    This reverts commit d6f23fe8f8f6927de023fdaa52477bb78f5325a3.

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import { GRAVITY } from './config.js'
 import { loadAssets } from './loader.js'
 import { mountTouchControls } from './touch.js'
 import { registerMenuScene }     from './scenes/menu.js'
-import { registerAboutScene }    from './scenes/about.js'
 import { registerLevel1Scene }   from './scenes/level1.js'
 import { registerCelebrationScene } from './scenes/celebration.js'
 import { registerGameOverScene } from './scenes/gameover.js'
@@ -33,7 +32,6 @@ loadAssets()
 mountTouchControls()
 
 registerMenuScene()
-registerAboutScene()
 registerLevel1Scene()
 registerCelebrationScene()
 registerGameOverScene()
